70095390618a74ed093ebf35095a994a9519ca5d
Rod <dev_rodts@outlook.com>
2025-01-29 09:17:37 -0300

    chore: configure eslint-love

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = (async function config() {
+    const { default: love } = await import('eslint-config-love')
+    return [
+      {
+        ...love,
+        files: ['**/*.js', '**/*.ts'],
+        parserOptions:{
+          ecmaVersion: 2019,
+          sourceType: "commomJS",
+          project: "./tsconfig.json"
+        }
+      },
+    ]
+  })()
